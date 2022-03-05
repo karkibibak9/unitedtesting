0948630f46c160d474d171fd404406e13c758f4a
karkibibak9 <47566089+karkibibak9@users.noreply.github.com>
2022-03-05 16:48:49 +0545

    error clean

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 import { Fragment } from 'react'
 import Facilty from '../components/faculty'
 // import styles from '../styles/Home.module.css'

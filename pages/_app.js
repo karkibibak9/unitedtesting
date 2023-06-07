5981e4ecaa62e2107008682b99c216e5c611e55f
unknown <karkibibak9@gmail.com>
2023-06-07 16:41:38 +0545

    google anylatics
    update app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,23 @@ import { Fragment } from 'react';
 import Footer from '../components/footer';
 import "../node_modules/react-bootstrap"
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
+      <Script
+      Strategy="afterInteractive"
+      src="https://www.googletagmanager.com/gtag/js?id=G-350HGSVJXR" 
+      />
+<Script id="google-anlytics" Strategy="afterInteractive" >
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-350HGSVJXR');
+  `}
+</Script>
       <Header />
       <Component {...pageProps} />
       <Footer />
